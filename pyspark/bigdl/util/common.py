40cd426d61a3a7e5daee5ce5e6a0efa992239f57
cyita <yitastudy@gmail.com>
2021-03-26 11:11:16 +0800

    update

--- pyspark/bigdl/util/common.py
+++ pyspark/bigdl/util/common.py
@@ -627,12 +627,9 @@ def _java2py(gateway, r, encoding="bytes"):
 
         if clsName in _picklable_classes:
             r = gateway.jvm.org.apache.spark.bigdl.api.python.BigDLSerDe.dumps(r)
-        elif isinstance(r, (JavaArray, JavaList)) and len(r) != 0 \
-                and isinstance(r[0], JavaObject) \
-                and r[0].getClass().getSimpleName() in ['DataFrame', 'Dataset']:
-            spark = get_spark_sql_context(get_spark_context())
-            r = list(map(lambda x: DataFrame(x, spark), r))
-        elif isinstance(r, (JavaArray, JavaList, JavaMap)):
+        elif isinstance(r, (JavaArray, JavaList)):
+            return list(map(lambda x: _java2py(gateway, x), r))
+        elif isinstance(r, JavaMap):
             try:
                 r = gateway.jvm.org.apache.spark.bigdl.api.python.BigDLSerDe.dumps(
                     r)
